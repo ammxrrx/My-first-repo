899e925e60b6576b59f181525e818c57a1341c5e
ammxrrx <ammar1great@gmail.com>
2024-12-12 00:14:43 +0500

    lolol

---
+++ hehe.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    cout << "hello" << endl;
+    int ammar;
+    cout << "Enter ammar" << endl;
+    cin >> ammar;
+    cout << "crazy meow hehe" << endl;
+}
